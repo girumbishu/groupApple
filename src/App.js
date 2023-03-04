342d5e9ec1983720d3da3ad463c69bae9dc2dcbf
girumbishu <girumbishu09@gmail.com>
2023-03-04 13:52:34 -0500

    netlify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Resources/css/bootstrap.css";
+import "./index.css";
+import Header from "./Components/Header/Header";
+import "./Components/Header/Header.css";
+import Alert from "./Components/Alert/Alert";
+import "./Components/Alert/Alert.css";
+import FirstHighlight from "./Components/FirstHighlight/FirstHighlight";
+import "./Components/FirstHighlight/FirstHighlight.css";
+import SecondHighlight from "./Components/SecondHighlight/SecondHighlight";
+import "./Components/SecondHighlight/SecondHighlight.css";
+import ThirdHighlight from "./Components/ThirdHighlightWrapper/ThirdHighlight";
+import "./Components/ThirdHighlightWrapper/thirdHighlight.css";
+import FourthHighlight from "./Components/FourthHighlightWrapper/FourthHighlight";
+import "./Components/FourthHighlightWrapper/fourthHighlight.css";
+import FifthHighlight from "./Components/FifthHighlightWrapper/FifthHighlight";
+import "./Components/FifthHighlightWrapper/fifthHighlight.css";
+import SixthHighlight from "./Components/SixthHighlightWrapper/SixthHighlight";
+import "./Components/SixthHighlightWrapper/sixthHighlight.css";
+import Footer from "./Components/Footer/Footer";
+import "./Components/Footer/footer.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Alert />
+            <FirstHighlight />
+            <SecondHighlight />
+            <ThirdHighlight />
+            <FourthHighlight />
+            <FifthHighlight />
+            <SixthHighlight />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
